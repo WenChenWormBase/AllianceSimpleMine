500ca16138d6a243d4d918ca51174de7f491e227
Wen Chen <wen@wormbase.org>
2022-04-21 13:24:32 -0700

    first commit

---
+++ prepSimpleMineFileStructure.sh
@@ -0,0 +1,13 @@
+cd /home/wen/agrSimpleMine/
+mkdir sourceFile
+cd sourceFile
+mkdir FB
+mkdir HUMAN
+mkdir MGI
+mkdir MIX
+mkdir RGD
+mkdir SGD
+mkdir WB
+mkdir ZFIN
+cp /home/wen/agrSimpleMine/bin/category_headers .
+
